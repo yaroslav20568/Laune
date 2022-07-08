ce7619321c425c55f22d5d7532b37d6837870951
Yaroslav <ravlushevich.iarek@mail.ru>
2022-07-08 09:52:33 +0300

    observer api

--- src/js/app.js
+++ src/js/app.js
@@ -1,35 +1,53 @@
 document.addEventListener('DOMContentLoaded', () => {
+	const intro = document.querySelector('.intro');
     const sectionHeaders = document.querySelectorAll('section .section__header');
     const mainSectionHeader = document.querySelector('.section__header-fixed');
     const mainSectionHeaderTitle = document.querySelector('.section__header-fixed .section__header-title');
 
     sectionHeaders.forEach(sectionHeader => {
-        sectionHeader.style.display = 'none';
+        // sectionHeader.style.display = 'none';
         const sectionHeaderTop = sectionHeader.parentElement.parentElement.getBoundingClientRect().top;
-        console.log(sectionHeaderTop);
-        
     });
 
     window.addEventListener('scroll', () => {
         console.log(document.documentElement.scrollTop);
-        if(document.documentElement.scrollTop > sectionHeaders[0].parentElement.parentElement.getBoundingClientRect().top) {
-            mainSectionHeader.style.display = 'block';
-            mainSectionHeaderTitle.textContent = 'О компании';
-        } else if(document.documentElement.scrollTop > sectionHeaders[0].parentElement.parentElement.getBoundingClientRect().top) {
-            mainSectionHeaderTitle.textContent = 'О продуктах';
-        } else {
-            mainSectionHeader.style.display = 'none';
+        if(document.documentElement.scrollTop > sectionHeaders[0].parentElement.parentElement.getBoundingClientRect().top + intro.clientHeight - mainSectionHeader.clientHeight) {
+			mainSectionHeader.classList.add('active');
+            // mainSectionHeaderTitle.textContent = 'О компании';
+        } else if(document.documentElement.scrollTop < sectionHeaders[0].parentElement.parentElement.getBoundingClientRect().top + intro.clientHeight - mainSectionHeader.clientHeight) {
+			mainSectionHeader.classList.remove('active');
         }
     });
 
-    // document.body.innerHTML += `
-    //     <div class="section__header-fixed">
-    //         <div class="container">
-    //             <div class="section__header">
-    //                 <h2 class="section__header-title">Цели сомпании</h2>
-    //                 <img src="img/section-logo.svg" alt="logo section" class="section__header-img">
-    //             </div>
-    //         </div>
-    //     </div>
-    // `;
+
+
+	const sections = [...document.querySelectorAll("section")];
+
+	let options = {
+		rootMargin: "0px",
+		threshold: 0.5
+	};
+
+	const callback = (entries, observer) => {
+		entries.forEach(entry => {
+			const { target } = entry;
+			
+			// if (entry.intersectionRatio >= 0.25) {
+			// 	console.log(target);
+			// } else {
+
+			// }
+			if(entry.isIntersecting) {
+				console.log(target);
+				mainSectionHeaderTitle.textContent = target.firstElementChild.firstElementChild.textContent;
+				console.log();
+			}
+		});
+	};
+
+	const observer = new IntersectionObserver(callback, options);
+
+	sections.forEach((section, index) => {
+		observer.observe(section);
+	});
 });
